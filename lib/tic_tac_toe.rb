c19f88b0194869370fc9e118efa0f60f28638011
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-03-12 08:38:26 +0300

    Running tests

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,5 +18,63 @@ class TicTacToe
         puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
     end
 
-    
+    # accept user input
+    # convert userinput to integer
+    # return a number -1 user_input
+    def input_to_index(user_input)
+        user_input.to_i - 1
+    end
+
+    # index,token
+    def move(index, token)
+        @board[index] = token
+    end
+
+    # the ? at the end of the method indicates that the method returns true || false
+    def position_taken?(index)
+        # # method 1
+        # return @board[index] !=  " "
+
+        # method 2:using if else statements
+        if @board[index] == " "
+            return false
+        else
+            return true
+        end
+    end
+
+    def valid_move?(index)
+        # method 1
+        !position_taken?(index) && index.between?(0,8)
+
+    #     # if else statement
+    #     if !position_taken?(index) && index.between?(0,8)
+    #         return true
+    #     else
+    #         return false
+    #     end
+    end
+
+    def turn_count
+        @board.count{|square| square !=  " "}
+    end
+
+    def current_player
+        turn_count.even? ? "X" : "O"
+    end
+
+    def turn
+       puts "Enter first move:" 
+       index = input_to_index(gets.chomp)
+       if valid_move?(index)
+        move(index, current_player)
+        display_board
+       else
+        turn
+    end
+    end
+
+    def won?
+        WIN_COMBINATIONS.include?
+    end
 end
